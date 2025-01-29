60c4aa5e8d369454cb790720f36e48ac19837420
dan2017558 <hexogaming123@gmail.com>
2025-01-28 23:06:46 -0600

    Back to Black & White
    This is the start of updating the terrain generation. Currently I have it rendered in only black and white in order to fully see all the details. The code so far only outputs a heightmap.
    
    I plan to make the terrain generation much more complex. However, until this point I've made the code much more readable and taken the colour out.
    
    Additionally added:
    .gitignore
    requirements
    
    Best settings tested so far:
    Persistence 0.7
    Lacunarity 2.5
    Default Frequency 1
    Octaves 6

--- main.py
+++ main.py
@@ -5,58 +5,59 @@ from perlin_noise import PerlinNoise
 import sys
 
 # Constants
-PIXELS = 640
-WIDTH = 640
-OCTAVES = (4, 16, 64, 256)
-COLOURS = {
-    "DARK_BLUE": (0, 0, 170),
-    "BLUE": (0, 0, 200),
-    "LIGHT_BLUE": (50, 50, 255),
-    "BEIGE": (255, 255, 0),
-    "LIGHT_GREEN": (0, 240, 0),
-    "GREEN": (0, 200, 0),
-    "DARK_GREEN": (0, 160, 0),
-    "GREY": (155, 155, 155),
-    "WHITE": (255, 255, 255)
-}
+PIXELS = 800
+GRID_SIZE = 800
+#DEFAULT_FREQUENCY = 4
+#PERSISTANCE = 0.5
+#LACUNARITY = 2
+#OCTAVES = 4
 
 # Initialize pygame and set the display
 pygame.init()
 pygame.display.set_caption("Terrain Generation")
 Display = pygame.display.set_mode((PIXELS, PIXELS))
+noise_grid = []
 
-# Create 4 noise layers by feeding set octaves into the PerlinNoise function
-Noise_Layers = [PerlinNoise(octaves=o) for o in OCTAVES]
 
-# 2d list with generated values between -1 & 1 exclusive
-World = [[(sum(1/(o+1) * Noise_Layers[o]([row/WIDTH, column/WIDTH]) for o in range(len(Noise_Layers)))) for column in range(WIDTH)] for row in range(WIDTH)]
+def create_noise(PERSISTANCE, LACUNARITY, DEFAULT_FREQUENCY, OCTAVES):
 
-# Define height thresholds and corresponding colours
-height_thresholds = [
-    (0, "DARK_BLUE"),
-    (8, "BLUE"),
-    (10, "LIGHT_BLUE"),
-    (15, "BEIGE"),
-    (20, "LIGHT_GREEN"),
-    (35, "GREEN"),
-    (50, "DARK_GREEN"),
-    (60, "GREY"),
-]
+    # Create Octave and frequencies
+    octave_frequencies = (DEFAULT_FREQUENCY*LACUNARITY**OCTAVE for OCTAVE in range(OCTAVES))
 
-# Maximum colour for heights above the last threshold
-default_colour = COLOURS["WHITE"]
+    # Create Perlin noise layers
+    noise_layers = [PerlinNoise(octaves=frequency) for frequency in octave_frequencies]
 
-# Assign colours to world cell values
-for row in range(WIDTH):
-    for column in range(WIDTH):
-        height = int(World[row][column] * 100)  # Integer with magnitude between 0-100 exlusive
-        Colour = default_colour  # Start with the default colour
+    # Generate grid of noise values
+    for grid_row in range(GRID_SIZE):
+        row_values = []
+        for grid_col in range(GRID_SIZE): 
+            combined_noise = 0  # Start with 0 as base avlue
+            
+            # Combine noise from all octaves (layers) into a single value
+            for layer_index in range(len(noise_layers)):
 
-        for threshold, colour in height_thresholds:
-            if height < threshold:
-                Colour = COLOURS[colour]
-                break
-        pygame.draw.rect(Display, Colour, Rect(column * (PIXELS / WIDTH), row * (PIXELS / WIDTH), (PIXELS / WIDTH), (PIXELS / WIDTH)))
+                amplitude = 1 * PERSISTANCE**(layer_index)
+                frequency_contribution = noise_layers[layer_index](
+                    [grid_row / GRID_SIZE, grid_col / GRID_SIZE]
+                )
+                
+                combined_noise += amplitude * frequency_contribution
+            
+            row_values.append(combined_noise)
+
+        noise_grid.append(row_values)
+
+
+def draw_noise(noise_grid):
+    for row in range(GRID_SIZE):
+        for column in range(GRID_SIZE):
+            height = int(abs(noise_grid[row][column]) * 100)
+            pygame.draw.rect(Display, (height, height, height), Rect(column * (PIXELS / GRID_SIZE), row * (PIXELS / GRID_SIZE), (PIXELS / GRID_SIZE), (PIXELS / GRID_SIZE)))
+
+
+# Create
+settings = str(input("Settings: ")).split()
+create_noise(float(settings[0]), float(settings[1]), float(settings[2]), int(settings[3]))
 
 # Display loop
 while True:
@@ -64,6 +65,8 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
+    draw_noise(noise_grid)
 
     pygame.display.update()
     pygame.time.Clock().tick(1)
